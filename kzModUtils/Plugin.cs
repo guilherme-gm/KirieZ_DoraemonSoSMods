7cb49b3e5c027d810307ed889b07a9ecdee3697e
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2022-10-12 22:41:21 -0300

    kzModUtils initial project (based on template)

---
+++ kzModUtils/Plugin.cs
@@ -0,0 +1,14 @@
+using BepInEx;
+
+namespace kzModUtils
+{
+	[BepInPlugin("io.github.guilherme-gm.DoraemonSoSMods.kzModUtils", "kz Mod Utils", PluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+		}
+	}
+}
