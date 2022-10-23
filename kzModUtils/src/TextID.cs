6642d8400a88ab04de816b14916fb106eb281872
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2022-10-23 17:06:41 -0300

    adds some TextID mappings in kzModUtils

---
+++ kzModUtils/src/TextID.cs
@@ -0,0 +1,51 @@
+namespace kzModUtils
+{
+	/**
+	 * Text IDs for ResourceModule.GetText()
+	 * 
+	 * The structure is based on original game's Define.Text but with readable names
+	 */
+	public static class TextID
+	{
+		public static class Common
+		{
+			public static readonly int TEXT_YES = 1700000000;
+
+			public static readonly int TEXT_NO = 1700000001;
+
+			public static readonly int TEXT_OK = 1700000002;
+
+			public static readonly int TEXT_CANCEL = 1700000003;
+
+			public static readonly int TEXT_CLOSE = 1700000004;
+
+			public static readonly int TEXT_SPRING = 1700000005;
+
+			public static readonly int TEXT_SUMMER = 1700000006;
+
+			public static readonly int TEXT_AUTUMN = 1700000007;
+
+			public static readonly int TEXT_WINTER = 1700000008;
+
+			public static readonly int TEXT_SUN = 1700000009;
+
+			public static readonly int TEXT_MON = 1700000010;
+
+			public static readonly int TEXT_TUE = 1700000011;
+
+			public static readonly int TEXT_WED = 1700000012;
+
+			public static readonly int TEXT_THU = 1700000013;
+
+			public static readonly int TEXT_FRI = 1700000014;
+
+			public static readonly int TEXT_SAT = 1700000015;
+
+			public static readonly int TEXT_INVENTORY = 1700000016;
+
+			public static readonly int TEXT_BACKPACK = 1700000017;
+
+			public static readonly int TEXT_STG_BOX = 1700000018;
+		}
+	}
+}
