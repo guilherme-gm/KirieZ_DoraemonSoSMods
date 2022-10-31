808d101ae4c97b7deabd8a079492b8e57bb86257
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2022-10-30 21:46:35 -0300

    showItemPrice: rework it to use new MessageBox element

--- ShowItemPrice/src/Plugin.cs
+++ ShowItemPrice/src/Plugin.cs
@@ -1,5 +1,9 @@
 using BepInEx;
 using HarmonyLib;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using kzModUtils.UI.Elements;
 
 namespace ShowItemPrice
 {
@@ -14,78 +18,64 @@ namespace ShowItemPrice
 			Harmony.CreateAndPatchAll(typeof(Plugin));
 		}
 
+		private static Dictionary<ItemExplainController, ItemPriceController> PriceControllers = new Dictionary<ItemExplainController, ItemPriceController>();
+
+		[HarmonyPatch(typeof(ItemExplainController), "Initialize")]
+		[HarmonyPrefix]
+		static void InitItemExplain(ItemExplainController __instance)
+		{
+			var rect = __instance.gameObject.GetComponent<RectTransform>();
+			if (rect == null)
+			{
+				Console.WriteLine("ItemExplainController doesn't have a RectTransform.");
+				return;
+			}
+
+			MessageBoxStyles style = MessageBoxStyles.Brown;
+			var chestController = __instance.transform.parent.transform.parent.transform.parent.GetComponent<ChestUIPartController>();
+			if (chestController != null) {
+				switch (chestController.StorageType) {
+					case Define.Storage.TypeEnum.Chest:
+						style = MessageBoxStyles.Green;
+						break;
+
+					case Define.Storage.TypeEnum.MaterialStoringSite:
+						style = MessageBoxStyles.Red;
+						break;
+
+					case Define.Storage.TypeEnum.Refrigerator:
+						style = MessageBoxStyles.Blue;
+						break;
+
+					case Define.Storage.TypeEnum.ShippingBox:
+						style = MessageBoxStyles.Purple;
+						break;
+				}
+			}
+
+			var msgBox = (new MessageBoxBuilder())
+				.SetParent(__instance.gameObject.transform)
+				.SetPosition(new Vector3(350, -83))
+				.SetSize(new Vector2(150, 140))
+				.SetTitle("Price Info")
+				.SetStyle(style)
+				.Build();
+			var controller = msgBox.gameObject.AddComponent<ItemPriceController>();
+			controller.Initialize(msgBox);
+
+			PriceControllers.Add(__instance, controller);
+		}
+
 		[HarmonyPatch(typeof(ItemExplainController), "SetItem")]
 		[HarmonyPrefix]
-		static bool SetItemWithPrice(
-			ItemExplainController __instance,
-			ItemModel item,
-			ref UnityEngine.UI.Text ___mTitleText,
-			ref UnityEngine.UI.Text ___mInfoText,
-			ref RateIconController ___mItemQuality,
-			ref UnityEngine.UI.Text ___mItemNumText
-		)
+		static void SetItemWithPrice(ItemExplainController __instance, ItemModel item)
 		{
-			if (item == null)
-			{
-				if (___mTitleText != null)
-				{
-					___mTitleText.text = string.Empty;
-				}
-				if (___mInfoText != null)
-				{
-					___mInfoText.text = string.Empty;
-				}
-				if (___mItemNumText != null)
-				{
-					___mItemNumText.transform.parent.gameObject.Deactivate();
-				}
-				if (___mItemQuality != null)
-				{
-					___mItemQuality.Deactivate();
-				}
-				__instance.Deactivate();
-			}
-			else
-			{
-				if (___mTitleText != null)
-				{
-					___mTitleText.text = item.Name;
-				}
-				if (___mInfoText != null)
-				{
-					___mInfoText.text = item.Description;
-					if (item.Size > 0f)
-					{
-						UnityEngine.UI.Text text = ___mInfoText;
-						text.text = text.text + "\n" + SingletonMonoBehaviour<MasterManager>.Instance.TextMaster.GetText(Define.Text.Common.ID_0000_0056) + TextUtility.GetSizeString(item.Id, item.Size);
-					}
-				}
-				if (___mItemNumText != null)
-				{
-					___mItemNumText.transform.parent.gameObject.Activate();
-					___mItemNumText.text = item.Count.ToString();
-				}
-				if (___mItemQuality != null)
-				{
-					if (item.Master.HasQuality)
-					{
-						___mItemQuality.SetRate(item.Quality);
-						___mItemQuality.Activate();
-					}
-					else
-					{
-						___mItemQuality.Deactivate();
-					}
-				}
-
-				if (item.SellingPrice != -1)
-				{
-					___mInfoText.text = ___mInfoText.text + "\nSell price: " + item.SellingPrice + " (Base: " + item.Master.BaseSellingPrice + ")";
-				}
-				__instance.Activate();
+			if (!PriceControllers.TryGetValue(__instance, out var controller)) {
+				Console.WriteLine("Could not find PriceController for Window");
+				return;
 			}
 
-			return false;
+			controller.SetItem(item);
 		}
 	}
 }
