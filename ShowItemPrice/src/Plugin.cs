1abf872056a845adf53ba2116f447d3a356787ba
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2022-11-01 23:26:14 -0300

    showItemPrice: fix interfaces where it is shown
    It no longer tries to render on collection books and have a
    more hand picked approach about which UIs to show.
    This way we avoid unexpected cases from breaking the game.

--- ShowItemPrice/src/Plugin.cs
+++ ShowItemPrice/src/Plugin.cs
@@ -20,10 +20,40 @@ namespace ShowItemPrice
 
 		private static Dictionary<ItemExplainController, ItemPriceController> PriceControllers = new Dictionary<ItemExplainController, ItemPriceController>();
 
+		private static MessageBoxStyles? GetBoxStyle(ItemExplainController __instance)
+		{
+			if (__instance?.transform?.parent?.transform?.parent?.transform?.parent?.GetComponent<InventoryUIPartController>() != null)
+				return MessageBoxStyles.Brown;
+
+			var chestController = __instance?.transform?.parent?.transform?.parent?.transform?.parent?.GetComponent<ChestUIPartController>();
+			if (chestController != null)
+			{
+				switch (chestController.StorageType)
+				{
+					case Define.Storage.TypeEnum.Chest:
+						return MessageBoxStyles.Green;
+
+					case Define.Storage.TypeEnum.MaterialStoringSite:
+						return MessageBoxStyles.Red;
+
+					case Define.Storage.TypeEnum.Refrigerator:
+						return MessageBoxStyles.Blue;
+
+					case Define.Storage.TypeEnum.ShippingBox:
+						return MessageBoxStyles.Purple;
+				}
+			}
+
+			return null;
+		}
+
 		[HarmonyPatch(typeof(ItemExplainController), "Initialize")]
 		[HarmonyPrefix]
 		static void InitItemExplain(ItemExplainController __instance)
 		{
+			if (PriceControllers.ContainsKey(__instance))
+				return;
+
 			var rect = __instance.gameObject.GetComponent<RectTransform>();
 			if (rect == null)
 			{
@@ -31,34 +61,16 @@ namespace ShowItemPrice
 				return;
 			}
 
-			MessageBoxStyles style = MessageBoxStyles.Brown;
-			var chestController = __instance.transform.parent.transform.parent.transform.parent.GetComponent<ChestUIPartController>();
-			if (chestController != null) {
-				switch (chestController.StorageType) {
-					case Define.Storage.TypeEnum.Chest:
-						style = MessageBoxStyles.Green;
-						break;
-
-					case Define.Storage.TypeEnum.MaterialStoringSite:
-						style = MessageBoxStyles.Red;
-						break;
-
-					case Define.Storage.TypeEnum.Refrigerator:
-						style = MessageBoxStyles.Blue;
-						break;
-
-					case Define.Storage.TypeEnum.ShippingBox:
-						style = MessageBoxStyles.Purple;
-						break;
-				}
-			}
+			MessageBoxStyles? style = GetBoxStyle(__instance);
+			if (style == null)
+				return;
 
 			var msgBox = (new MessageBoxBuilder())
 				.SetParent(__instance.gameObject.transform)
 				.SetPosition(new Vector3(350, -83))
 				.SetSize(new Vector2(150, 140))
 				.SetTitle("Price Info")
-				.SetStyle(style)
+				.SetStyle((MessageBoxStyles) style)
 				.Build();
 			var controller = msgBox.gameObject.AddComponent<ItemPriceController>();
 			controller.Initialize(msgBox);
